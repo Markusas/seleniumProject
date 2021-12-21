2c429b484bb768b711de5ea325121346a1f74207
Markusas <saltinio@gmail.com>
2021-12-21 12:58:03 +0200

    Create test class

---
+++ src/main/java/example/selenium/Run.java
@@ -0,0 +1,14 @@
+package main.java.example.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Run {
+    public static void main(String[] args) {
+
+        //Invoking Browser
+        //Chrome - ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\632789\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    }
+}
