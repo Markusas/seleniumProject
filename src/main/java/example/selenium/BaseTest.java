d57bf721644dbfc18eafffd05563763571bcd3ba
Markusas <saltinio@gmail.com>
2022-01-17 12:15:05 +0200

    Changed wait

--- src/main/java/example/selenium/BaseTest.java
+++ src/main/java/example/selenium/BaseTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     public WebDriver driver;
@@ -32,7 +33,7 @@ public class BaseTest {
         options.setExperimentalOption("useAutomationExtension", false);
 
         driver.get(baseUrl);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
