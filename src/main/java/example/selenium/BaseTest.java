97f06959c6cdc6edfa6052a32ca9166837735d15
Markusas <saltinio@gmail.com>
2022-01-17 14:15:43 +0200

    Small changes 3

--- src/main/java/example/selenium/BaseTest.java
+++ src/main/java/example/selenium/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-dev-shm-usage");
         //options.setExperimentalOption("useAutomationExtension", false);
         driver = new ChromeDriver();
         driver.get(baseUrl);
